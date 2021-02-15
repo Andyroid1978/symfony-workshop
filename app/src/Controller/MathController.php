08e74ef44af53878e4ab378de3aed5ab774dc309
andy <a.badii@open-box.it>
2021-02-15 12:08:18 +0100

    esercizio

---
+++ app/src/Controller/MathController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+
+class MathController extends AbstractController
+{
+
+    public function sum(int $value1, int $value2)
+    {
+        $resp = [
+            'value1' => $value1,
+            'value2' => $value2,
+            'sum' => $value1 + $value2,
+        ];
+
+        return new JsonResponse($resp);
+    }
+}
